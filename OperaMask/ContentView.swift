632b4a079ec40029fba446a984b1ce1a6d127526
sophiaweng <sophiasophiaweng@gmail.com>
2020-11-16 11:06:54 +0800

    Initial Commit

---
+++ OperaMask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OperaMask
+//
+//  Created by 翁淑惠 on 2020/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
